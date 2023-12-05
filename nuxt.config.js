94edf34863d8b7ced3b79ec63c0264d22be2c8bb
Marco Ferrari <marco.ferrari@me.com>
2023-12-05 11:29:05 +0100

    change title of the website

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'interspecies-money-test',
+    title: 'Interspecies Money',
     htmlAttrs: {
       lang: 'en',
     },
