e2ab5f6079b3bce3b46943fb225208d5784ad9a9
Angelo Semeraro <angelose@MacBook-Pro-di-Angelo.local>
2023-12-19 18:36:20 +0100

    Removing unused files

--- .stylelintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
-    plugins: ['stylelint-order'], // If you are using stylelint-order
-    rules: {
-      // Add or override rules as needed
-    },
-  };
-  
--- deploy.sh
+++
@@ -1,1 +0,0 @@
-rsync -avh dist/ angelose@107.6.161.162:/home/angelose/public_html/interspecies-money/ --delete
